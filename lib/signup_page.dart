16e03a0a0a7c2bd0f1d037fe8fbf596012aced9a
sankalp <sankalppandey696@gmail.com>
2024-10-05 03:37:43 +0530

    learning to use input field and rich text

---
+++ lib/signup_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class SignupPage extends StatefulWidget{
+  const SignupPage({super.key});
+  @override
+  _SignupPageState createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage>{
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Padding(padding: EdgeInsets.all(12) , 
+        
+        )
+      ],
+    );
+  }
+}
